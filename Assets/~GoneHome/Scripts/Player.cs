043a8c64f5818312356897893087ff1d1a4a63df
thechilledgamer95 <thechilledgamer@gmail.com>
2018-04-10 15:59:51 +1000

    Added player and goal script

--- Assets/~GoneHome/Scripts/Player.cs
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -4,18 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody rigid;
+    public float speed = 10f;
+    public float jumpHeight = 15f;
+
+    private Rigidbody rigid;
 
      
     // Use this for initialization
     void Start()
     {
-
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Move();
+        Jump();
+    }
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigid.AddForce(Vector3.back * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigid.AddForce(Vector3.left * speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigid.AddForce(Vector3.right * speed);
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
     }
 }
+           
