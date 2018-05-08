3db52683ba75e87c256fb76f6acda64c2ee1fdb7
thechilledgamer95 <thechilledgamer@gmail.com>
2018-05-08 15:57:51 +1000

    Created Astroids game

---
+++ unity/Assets/~Asteroids/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class Movement : MonoBehaviour
+    {
+        public float accelerate = 50f;
+        public float rotationSpeed = 360f;
+
+        private Rigidbody2D rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+        }
+
+        void Accelerate()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            rigid.AddForce(transform.up * inputV * accelerate);
+        }
+
+        void Rotate()
+        {
+            float inputH = Input.GetAxis("Horizontal");
+            transform.Rotate(Vector3.back, rotationSpeed * inputH * Time.deltaTime);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Accelerate();
+            Rotate();
+        }
+
+
+    }
+}
---
+++ unity/Assets/~Asteroids/Scripts/Shooting.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class Shooting : MonoBehaviour
+    // Use this for initialization
+    {
+        public GameObject bulletPrefab;
+        public float bulletSpeed = 20f;
+        public float shootRate = 0.2f;
+
+        private float shootTimer = 0f;
+        void Update()
+        {
+            shootTimer += Time.deltaTime;
+            if (shootTimer >= shootRate)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    Shoot();
+                    shootTimer = 0f; 
+                }
+            }
+        }
+        void Shoot()
+        {
+            GameObject clone = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            Rigidbody2D rigid = clone.GetComponent<Rigidbody2D>();
+            rigid.AddForce(transform.up * bulletSpeed, ForceMode2D.Impulse);
+        }
+
+        // Update is called once per frame
+    }
+}
