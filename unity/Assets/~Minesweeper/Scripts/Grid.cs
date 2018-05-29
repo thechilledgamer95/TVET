c6b23d47d75a4ac32660f47e2eae3b36b9bb49bf
thechilledgamer95 <thechilledgamer@gmail.com>
2018-05-29 16:02:29 +1000

    Worked on part 2 of Minesweeper

--- unity/Assets/~Minesweeper/Scripts/Grid.cs
+++ unity/Assets/~Minesweeper/Scripts/Grid.cs
@@ -6,7 +6,95 @@ namespace Minesweeper
 {
     public class Grid : MonoBehaviour
     {
-        
+        public GameObject tilePrefab;
+        public int width = 10, height = 10;
+        public float spacing = .115f;
+
+        private Tile[,] tiles;
+
+
+        Tile SpawnTile(Vector3 pos)
+        {
+            GameObject clone = Instantiate(tilePrefab);
+            clone.transform.position = pos; ;
+            Tile currentTile = clone.GetComponent<Tile>();
+            return currentTile;
+        }
+
+        void GenerateTiles()
+        {
+            tiles = new Tile[width, height];
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    Vector2 halfSize = new Vector2(width * 0.5f,
+                                                   height * 0.5f);
+                    Vector2 pos = new Vector2(x - halfSize.x,
+                                              y - halfSize.y);
+
+                    pos *= spacing;
+                    Tile tile = SpawnTile(pos);
+                    tile.transform.SetParent(transform);
+                    tile.x = x;
+                    tile.y = y;
+                    tiles[x, y] = tile;
+                }
+            }
+        }
+
+        void Start()
+        {
+            GenerateTiles();
+        }
+
+        public int GetAdjacentMineCount(Tile tile)
+        {
+            int count = 0;
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    int desiredX = tile.x + x;
+                    int desiredY = tile.y + y;
+                    if (desiredX < 0 || desiredX >= width ||
+                        desiredY < 0 || desiredY >= height)
+                    {
+                        continue;
+                    }
+                    Tile currentTile = tiles[desiredX, desiredY];
+                    if (currentTile.isMine)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        void SelectATile()
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(mouseRay.origin,
+                                                 mouseRay.direction);
+            if (hit.collider != null)
+            {
+                Tile hitTile = hit.collider.GetComponent<Tile>();
+                if (hitTile != null)
+                {
+                    int adjacentMines = GetAdjacentMineCount(hitTile);
+                    hitTile.Reveal(adjacentMines);
+                }
+            }
+        }
+
+        void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                SelectATile();
+            }
+        }
     }
 
 }
